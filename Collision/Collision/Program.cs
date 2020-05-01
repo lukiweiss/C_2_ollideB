9be0b4103ab4779da1c0a30755c2d5ec3a29297e
Hanspeter Umilietti <umihan@rgtfo-me.it>
2020-05-01 16:37:16 +0200

    1. Commit

---
+++ Collision/Collision/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+/*
+ *  Collide
+ *  Eine Simulation im 2-dimensionalen Raum
+ * 
+ * 
+ * 
+ * 2020 TFO-Meran
+ */
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        const int seite = 50;
+        static int[,] feld = new int[seite, seite];
+
+        class einer
+        {
+            // Private Eigenschaften
+
+            // Öffentliche Eigenschaften
+            public int posx, posy;
+            public ConsoleColor farbe;
+            // Konstruktor
+            public einer()
+            {
+            }
+            //Private Methoden
+            void show()
+            {
+            }
+            void hide()
+            {
+            }
+            void collide()
+            {
+            }
+            //Öffentliche Methoden
+            public void Move()
+            {
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = seite*2;
+            Console.WindowHeight = seite;
+            Console.Clear();
+            Random ZG = new Random();
+            int Anzahl=ZG.Next(1,6);
+            einer[] meineEiner = new einer[Anzahl];
+            for (int i = 0; i < Anzahl; i++)
+            {
+                meineEiner[i] = new einer();
+            }
+            Console.CursorVisible = false;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < Anzahl; j++)
+                {
+                    meineEiner[j].Move();
+                }
+                System.Threading.Thread.Sleep(10);
+
+            }
+        }
+    }
+}
